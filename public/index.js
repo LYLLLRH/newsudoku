66a7a76f530524a1f2b54e5b75bd5317116fbaa0
LLLRH <lylllrh@163.com>
2019-05-29 08:50:55 +0800

    One number in Multi-position Erase

--- public/index.js
+++ public/index.js
@@ -32,6 +32,23 @@ function getNumberInCol(cells,col) {
 	return numStr;
 }
 
+
+function clearNum_incol(cells,block,col,num) {
+	for(var i=blockStart[block][0];i<blockStart[block][0]+3;i++) {
+		if (cells[i][col].length>1) {
+			cells[i][col] = cells[i][col].replace(num,'')
+		}
+	}
+}
+
+function clearNum_inrow(cells,block,row,num) {
+	for(var j=blockStart[block][1];j<blockStart[block][1]+3;j++) {
+		if (cells[row][j].length>1) {
+			cells[row][j] = cells[row][j].replace(num,'')
+		}
+	}
+}
+
 function cells2Str(cells,possiblity) {
 	var str = '';
 	for (var i=0;i<9;i++) {
@@ -192,7 +209,7 @@ Sudoku.prototype.reverseRow = function(row) {
 	 	console.log("reverseRow:row err");		
 	}
 	return change;
-};
+};	
 
 Sudoku.prototype.reverseCol = function(col) {
 	var match;
@@ -306,6 +323,52 @@ Sudoku.prototype.reverse2Row = function(row) {
 
 }
 
+Sudoku.prototype.reverse3Row = function(row) {
+	var match;
+	var change =0;
+	var i='';
+	if (row < 9 && row>=0) {
+		for(var num1=1;num1<=7;num1++) { //Test whether number is unique in a row 
+			for (var num2=num1+1;num2<=8;num2++ ) {
+				for (var num3=num3+1;num3<=9;numb3++) {
+
+
+					i= num1+''+num2+num3;
+					match = [];
+					for (var j=0;j<9;j++) {
+						if (this.cells[row][j] === i ) {			
+							match.push(j);
+						}
+					}
+					if ( match.length===3) { 
+						for (var j=0;j<9;j++) {
+							if (this.cells[row][j].length > 1 && j!==match[0] && j!=match[1] && j!==match[2]) {			
+				 				var str = this.cells[row][j].replace(num1,'');
+				 				str = str.replace(num2,'');
+				 				str = str.replace(num3,'');
+		 						change += str==this.cells[row][j]?0:1;
+		 						this.cells[row][j] =str;
+				 				if (str.length === 1) {this.solution.push({
+				 					cellsStr: cells2Str(this.cells),
+				 					method: 'reverse3Row',
+				 					col: j,
+				 					row: row,
+				 					num:str }
+				 					)}	
+							}
+					}
+						return change;
+					}	
+				}		
+			}
+		}	
+	} else {
+	 	console.log("reverse2Row:row err");		
+	}
+	return change;
+
+}
+
 
 Sudoku.prototype.reverse2Col = function(col) {
 	var match;
@@ -339,6 +402,50 @@ Sudoku.prototype.reverse2Col = function(col) {
 				}
 					return change;
 				}	
+			}
+		}	
+	} else {
+	 	console.log("reverse2Col:row err");		
+	}
+	return change;
+
+}
+
+Sudoku.prototype.reverse3Col = function(col) {
+	var match;
+	var change =0;
+	var i='';
+	if (col < 9 && col>=0) {
+		for(var num1=1;num1<=7;num1++) { //Test whether number is unique in a row 
+			for (var num2=num1+1;num2<=8;num2++ ) {
+				for (var num3=num2+1;num3<=9;num3++) {
+					i= num1+''+num2+num3;
+					match = [];
+					for (var j=0;j<9;j++) {
+						if (this.cells[j][col] === i ) {			
+							match.push(j);
+						}
+					}
+					if ( match.length===3) { 
+						for (var j=0;j<9;j++) {
+							if (this.cells[j][col].length > 1 && j!==match[0] && j!=match[1] && j!==match[2]) {			
+				 				var str = this.cells[j][col].replace(num1,'');
+				 				str = str.replace(num2,'');
+				 				str = str.replace(num3,'')
+		 						change += str==this.cells[j][col]?0:1;
+		 						this.cells[j][col] =str;
+				 				if (str.length === 1) {this.solution.push({
+				 					cellsStr: cells2Str(this.cells),
+				 					method: 'reverse3Col',
+				 					col: col,
+				 					row: j,
+				 					num:str }
+				 					)}		 						
+							}
+					}
+						return change;
+					}
+				}		
 			}
 		}	
 	} else {
@@ -392,6 +499,161 @@ Sudoku.prototype.reverse2Block = function(block) {
 	return change;
 
 }
+
+Sudoku.prototype.reverse3Block = function(block) {
+	var match;
+	var change =0;
+	var i='';
+	if (block < 9 && block>=0) {
+		for(var num1=1;num1<=7;num1++) { //Test whether number is unique in a row 
+			for (var num2=num1;num2<=8;num2++ ) {
+				for (var num3=num2+1;num3<=9;num3++){
+					i= num1+''+num2+num3;
+					match = [];
+					for (var j=blockStart[block][0];j<blockStart[block][0]+3;j++) {
+						for (var t=blockStart[block][1];t<blockStart[block][1]+3;t++) {
+							if (this.cells[j][t] === i ) {			
+								match.push([j,t]);
+							}
+						}	
+					}
+					if ( match.length===3) { 
+						for (var j=blockStart[block][0];j<blockStart[block][0]+3;j++) {
+							for (var t=blockStart[block][1];t<blockStart[block][1]+3;t++) {
+								if (this.cells[j][t].length > 1 &&  j+''+t !== match[0].join('') && j+''+t !== match[1].join('') && j+''+t !== match[2].join(''))  {			
+					 				var str = this.cells[j][t].replace(num1,'');
+					 				str = str.replace(num2,'');
+					 				str = str.replace(num3,'')
+			 						change += str==this.cells[j][t]?0:1;
+			 						this.cells[j][t] =str;
+					 				if (str.length === 1) {this.solution.push({
+					 					cellsStr: cells2Str(this.cells),
+					 					method: 'reverse3Block',
+					 					col: t,
+					 					row: j,
+					 					num:str }
+					 					)}			 						
+								}
+							}
+						}	
+						return change;
+					}	
+				}
+			}
+		}	
+	} else {
+	 	console.log("reverse2Block:row err");		
+	}
+	return change;
+
+}
+
+// 加上在一个Block中的，某个数字指示存在一列中，那在这一列的其他的2个Block中，这是数字是已经被占用的了
+Sudoku.prototype.mulnumn_inacol = function (block) {
+	// 检查这个Number是仅仅存在于这个Block的这列中
+	for (var i=blockStart[block][0];i<blockStart[block][0]+3;i++) {
+		for (var j=blockStart[block][1];j<blockStart[block][1]+3;j++) {
+			if (this.cells[i][j].length==1) {
+				continue;
+			}
+			// console.log(this.cells[i][j])
+			for(var k=0;k<this.cells[i][j].length;k++) {
+				console.log(i+' '+j);
+				// console.log((this.celles[i][j]));
+				console.log('result:'+checkmulnum_inacol(this.cells,block,j,this.cells[i][j][k]))
+				if (checkmulnum_inacol(this.cells,block,j,this.cells[i][j][k])) {
+					clearNum_incol(this.cells,(block+3)%9,j,this.cells[i][j][k]);
+					clearNum_incol(this.cells,(block+6)%9,j,this.cells[i][j][k]);
+				}
+			}
+		} 
+	}
+
+}
+
+// 加上在一个Block中的，某个数字指示存在一行中，那在这一行的其他的2个Block中，这是数字是已经被占用的了
+Sudoku.prototype.mulnumn_inarow = function (block) {
+	// 检查这个Number是仅仅存在于这个Block的这行中
+	for (var i=blockStart[block][0];i<blockStart[block][0]+3;i++) {
+		for (var j=blockStart[block][1];j<blockStart[block][1]+3;j++) {
+			if (this.cells[i][j].length==1) {
+				continue;
+			}
+			// console.log(this.cells[i][j])
+			for(var k=0;k<this.cells[i][j].length;k++) {
+				// console.log(i+' '+j);
+				// console.log((this.celles[i][j]));
+				// console.log('result:'+checkmulnum_inacol(this.cells,block,j,this.cells[i][j][k]))
+				if (checkmulnum_inarow(this.cells,block,i,this.cells[i][j][k])) {
+					clearNum_inrow(this.cells,(block+1)%3+3*parseInt(block/3),i,this.cells[i][j][k]);
+					clearNum_inrow(this.cells,(block+2)%3+3*parseInt(block/3),i,this.cells[i][j][k]);
+				}
+			}
+		} 
+	}
+
+}
+
+function checkmulnum_inarow(cells,block,x,num) {   // x是行,Number 具体数字
+	 var match = 0;
+	 // console.log('block:'+block);
+	 // console.log(y+'--'+num)
+     for (var i=blockStart[block][0];i<blockStart[block][0]+3;i++) {
+ 		if (i==x) {  // 如何和J是同一行，需要Check有没有这个Num，如果没有返回False，如果有就继续		
+     		for (var j=blockStart[block][1];j<blockStart[block][1]+3;j++) {
+     			if (cells[i][j].length>1) {
+     				if (cells[i][j].indexOf(num)>=0) {
+     					match = 1;
+     					break;
+     				}
+     			}
+     		}
+     		// console.log("Match:"+match)
+     		if (match==0) {
+     			return 0;
+     		} else {
+     			continue;
+     		}
+     		
+     	}
+     	for (j=blockStart[block][1];j<blockStart[block][1]+3;j++) {
+     		if (cells[i][j].indexOf(num) >=0) {return 0;}
+     	}
+
+     }
+     return 1;
+}
+
+function checkmulnum_inacol(cells,block,y,num) {   // y是列，Number 具体数字
+	 var match = 0;
+	 // console.log('block:'+block);
+	 console.log(y+'--'+num)
+     for (var j=blockStart[block][1];j<blockStart[block][1]+3;j++) {
+ 		if (j==y) {  // 如何和J是同一列，需要Check有没有这个Num，如果没有返回False，如果有就继续		
+     		for (var i=blockStart[block][0];i<blockStart[block][0]+3;i++) {
+     			if (cells[i][j].length>1) {
+     				if (cells[i][j].indexOf(num)>=0) {
+     					match = 1;
+     					break;
+     				}
+     			}
+     		}
+     		console.log("Match:"+match)
+     		if (match==0) {
+     			return 0;
+     		} else {
+     			continue;
+     		}
+     		
+     	}
+     	for (i=blockStart[block][0];i<blockStart[block][0]+3;i++) {
+     		if (cells[i][j].indexOf(num) >=0) {return 0;}
+     	}
+
+     }
+     return 1;
+}
+
 
 Sudoku.prototype.checkValid= function() {
 	var all ;
